efd007b79b32ad54978feafb97e002771e349072
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-02-18 19:07:18 -0800

    storing tasks in a collection

--- simple-todos.js
+++ simple-todos.js
@@ -1,13 +1,25 @@
-if (Meteor.isClient){
-  //this code only runs on the client
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      {text: "This is task 1"},
-      {text: "This is task 2"},
-      {text: "This is task 3"}
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
+
+
+// if (Meteor.isClient){
+//   //this code only runs on the client
+//   Template.body.helpers({
+//     tasks: [
+//       {text: "This is task 1"},
+//       {text: "This is task 2"},
+//       {text: "This is task 3"}
+//     ]
+//   });
+// }
 // if (Meteor.isClient) {
 //   // counter starts at 0
 //   Session.setDefault('counter', 0);
