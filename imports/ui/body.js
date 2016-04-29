e05734f4f72fd5e262726a12433640a5133df4bd
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-28 20:04:40 -0700

    tutorial changed I believe trying to get the right directory structure

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,24 @@
 import { Template } from 'meteor/templating';
 
-import { Tasks } from '../api/tasks/.js';
-
 import './body.html';
 
 Template.body.helpers({
-    tasks() {
-        return Tasks.find({});
-    },
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
 });
+
+
+// import { Template } from 'meteor/templating';
+
+// import { Tasks } from '../api/tasks/.js';
+
+// import './body.html';
+
+// Template.body.helpers({
+//     tasks() {
+//         return Tasks.find({});
+//     },
+// });
