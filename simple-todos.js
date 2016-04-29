ef49899afcb23951e3689d61df942ebca4829805
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-28 19:43:38 -0700

    comment out the simple-todos.js

--- simple-todos.js
+++ simple-todos.js
@@ -1,68 +1,70 @@
-Tasks = new Mongo.Collection("tasks");
+//error Task.insert is already defined
 
-if (Meteor.isClient){
-
-  Template.body.helpers({
-    tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
-
-  //add event handler for form submit
-
-  Template.body.events({
-    "submit .new-task": function(event){
-      // Prevent default browser for submit
-      event.preventDefault();
-
-      //get value from form element
-
-      var text = event.target.text.value;
-
-      // insert a task into the colleciton
-
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
-
-      //clear form
-      event.target.text.value = "";
-    }
-  });
-}
-
+// Tasks = new Mongo.Collection("tasks");
 
 // if (Meteor.isClient){
-//   //this code only runs on the client
+
 //   Template.body.helpers({
-//     tasks: [
-//       {text: "This is task 1"},
-//       {text: "This is task 2"},
-//       {text: "This is task 3"}
-//     ]
-//   });
-// }
-// if (Meteor.isClient) {
-//   // counter starts at 0
-//   Session.setDefault('counter', 0);
-
-//   Template.hello.helpers({
-//     counter: function () {
-//       return Session.get('counter');
+//     tasks: function() {
+//       return Tasks.find({}, {sort: {createdAt: -1}});
 //     }
 //   });
 
-//   Template.hello.events({
-//     'click button': function () {
-//       // increment the counter when button is clicked
-//       Session.set('counter', Session.get('counter') + 1);
+//   //add event handler for form submit
+
+//   Template.body.events({
+//     "submit .new-task": function(event){
+//       // Prevent default browser for submit
+//       event.preventDefault();
+
+//       //get value from form element
+
+//       var text = event.target.text.value;
+
+//       // insert a task into the colleciton
+
+//       Tasks.insert({
+//         text: text,
+//         createdAt: new Date() // current time
+//       });
+
+//       //clear form
+//       event.target.text.value = "";
 //     }
 //   });
 // }
 
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
+
+// // if (Meteor.isClient){
+// //   //this code only runs on the client
+// //   Template.body.helpers({
+// //     tasks: [
+// //       {text: "This is task 1"},
+// //       {text: "This is task 2"},
+// //       {text: "This is task 3"}
+// //     ]
+// //   });
+// // }
+// // if (Meteor.isClient) {
+// //   // counter starts at 0
+// //   Session.setDefault('counter', 0);
+
+// //   Template.hello.helpers({
+// //     counter: function () {
+// //       return Session.get('counter');
+// //     }
+// //   });
+
+// //   Template.hello.events({
+// //     'click button': function () {
+// //       // increment the counter when button is clicked
+// //       Session.set('counter', Session.get('counter') + 1);
+// //     }
+// //   });
+// // }
+
+// // if (Meteor.isServer) {
+// //   Meteor.startup(function () {
+// //     // code to run on server at startup
+// //   });
+// // }
